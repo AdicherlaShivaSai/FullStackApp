a352aa553476f931e85c263537cec2eb0ca7cd50
AdicherlaShivaSai <67548870+AdicherlaShivaSai@users.noreply.github.com>
2025-06-05 14:30:39 +0530

    Update Likes.js

--- server/models/Likes.js
+++ server/models/Likes.js
@@ -1,5 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    const Likes = sequelize.define('Likes') 
+  const Likes = sequelize.define("Likes", {
+    // Your other fields like PostId, etc.
+  });
 
-    return Likes;
-}
+  Likes.associate = (models) => {
+    Likes.belongsTo(models.Users, {
+      foreignKey: {
+        name: 'fk_likes_user', // <-- custom FK name (must be unique)
+        allowNull: false
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+
+    // If Likes also belongs to Post:
+    Likes.belongsTo(models.Posts, {
+      foreignKey: {
+        name: 'fk_likes_post',
+        allowNull: false
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  };
+
+  return Likes;
+};
