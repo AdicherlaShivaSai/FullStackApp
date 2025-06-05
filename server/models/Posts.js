425473944cfd71ff19ca5ebeb8a382960d111ccc
AdicherlaShivaSai <67548870+AdicherlaShivaSai@users.noreply.github.com>
2025-06-05 14:09:37 +0530

    Update Posts.js

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,34 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-    const Posts = sequelize.define('Posts', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        postText: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    Posts.associate = (models) => {
-        Posts.hasMany(models.Comments, {
-            onDelete: 'cascade'
-        });
-        Posts.hasMany(models.Likes, {
-            onDelete: 'cascade'
-        });
-    }
-    Posts.belongsTo(Users, {
-          foreignKey: {
-            name: 'fk_user_id', // <- give it a unique name
-            allowNull: true
-          },
-          onDelete: 'CASCADE',
-          onUpdate: 'CASCADE'
-        });
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-    return Posts;
-}
+  Posts.associate = (models) => {
+    Posts.belongsTo(models.Users, {
+      foreignKey: {
+        name: 'fk_user_id',
+        allowNull: true,
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  };
+
+  return Posts;
+};
