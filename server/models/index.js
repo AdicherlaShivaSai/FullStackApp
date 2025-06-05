6792fd5671f2d64402c5cdfeac1854352140130c
AdicherlaShivaSai <67548870+AdicherlaShivaSai@users.noreply.github.com>
2025-06-05 12:58:37 +0530

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -6,15 +6,24 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+require('dotenv').config(); // Load from .env
+
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: false // db4free does not require SSL; use true only if required
+    },
+    logging: false,
+  }
+);
 
 fs
   .readdirSync(__dirname)
